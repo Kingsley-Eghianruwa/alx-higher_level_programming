52cb36410611a74d1fc87196f4464ce1a3525fdd
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-06-27 01:13:35 +0000

    updated 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l_digit = number % 10
+l_digit = 0
+# beginif
+if (number >= 0):
+    l_digit = number % 10
+else:
+    l_digit = number % -10
+# endif
 str = "Last digit of "
 # beginif
 if (l_digit > 5):
@@ -9,5 +15,6 @@ if (l_digit > 5):
 elif (l_digit == 0):
     print(str + "{} is {} and is 0".format(number, l_digit))
 elif (l_digit < 6 and l_digit != 0):
-    print(str + "{} is {} and is less than 6 and not 0".format(number, l_digit))
+    l_d = l_digit  # done to make line 18 less tha 80 characters
+    print(str + "{} is {} and is less than 6 and not 0".format(number, l_d))
 # endif
