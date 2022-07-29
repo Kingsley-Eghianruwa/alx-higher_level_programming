8248627e54730ab8068e5909403ce80e939d5ca1
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-07-29 13:57:57 +0000

    added 1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1 as cal
+    a = 10
+    b = 5
+    tmp = 0
+    
+    tmp = cal.add(a, b)
+    print("{} + {} = {}".format(a, b, tmp))
+    tmp = cal.sub(a, b)
+    print("{} - {} = {}".format(a, b, tmp))
+    tmp = cal.mul(a, b)
+    print("{} * {} = {}".format(a, b, tmp))
+    tmp = cal.div(a, b)
+    print("{} / {} = {}".format(a, b, tmp))
