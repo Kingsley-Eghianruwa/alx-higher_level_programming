d8e6a06c62bd4afeefe5fa1af37a3b09a4f040bf
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-01 22:33:22 +0000

    major rewrite 2

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-import importlib as libm;writer = libm.import_module("sys");writer.stdout.write("#pythoniscool" + '\n')
+wr = __import__("sys")
+wr.stdout.write("#pythoniscool" + '\n')
