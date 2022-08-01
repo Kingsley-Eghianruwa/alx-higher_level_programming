8a22732de129c67e3a83581a77052ce181d25e24
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-01 22:40:17 +0000

    a minor change

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-import importlib as Ilib; wr = Ilib.import_module("sys")
-wr.stdout.write("#pythoniscool" + '\n')
+import importlib as Ilib; wr = Ilib.import_module("sys"); wr.stdout.write("#pythoniscool" + '\n')
