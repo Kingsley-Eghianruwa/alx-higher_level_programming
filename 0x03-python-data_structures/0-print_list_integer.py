a8c21eff32e549342a749a868985389e4767f797
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-03 17:43:51 +0000

    minor update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,7 +4,7 @@ def print_list_integer(my_list=[]):
     l_max_idx = len(my_list) - 1
     index = 0
     while (index <= l_max_idx):
-        print("{}".format(my_list[index]))
+        print("{:d}".format(my_list[index]))
         index = index + 1
     #  endwhile
 #  enddef
