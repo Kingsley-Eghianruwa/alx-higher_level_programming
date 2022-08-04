1ec58d151ead68baf8b0078b598ce0c6d76c70b0
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-04 16:33:21 +0000

    a small update

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if (my_list == None):
+    if (my_list is None):
         return None
     else:
         max_idx = len(my_list) - 1
-    
+
     index = max_idx
 
     while (index >= 0):
