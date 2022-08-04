a3f3d9f3d07ebb2a3f4cb8bd202c00e5f82798ab
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-04 16:24:15 +0000

    added 3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    max_idx = len(my_list) - 1
+    index = max_idx
+    while (index >= 0):
+        print("{:d}".format(my_list[index]))
+        index = index - 1
+    #  endwhile
+#  enddef
