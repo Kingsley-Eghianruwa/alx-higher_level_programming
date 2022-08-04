8eac96673263857c13dd2f8d1225c1057f7a2d2a
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-04 15:49:08 +0000

    added 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    max_idx = len(my_list) - 1
+
+    if (idx > max_idx or idx < 0):
+        return None
+    else:
+        return(my_list[idx])
+#  enddef
