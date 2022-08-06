212fa64411bff04473cbdee4831334f9b84634e7
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-06 15:52:39 +0000

    added 11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if (len(my_list) == 0):
+        return None
+    else:
+        del my_list[idx]
+        return my_list
+    #  endif
+#  enddef:delete_at
