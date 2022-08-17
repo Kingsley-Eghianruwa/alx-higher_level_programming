fe4c53471fb2024f9cbf9043957e82bb3ab86777
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-17 19:48:43 +0000

    added 6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    keys_list = sorted(a_dictionary)
+    idx = 0;
+    #  ###
+    while (idx < len(keys_list)):
+        print("{}: {}".format(keys_list[idx], a_dictionary[keys_list[idx]]))
+        idx = idx + 1
+    #  endwhile
