63d62354607b1727a1855e58a0ea8544d62a863b
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-17 13:40:15 +0000

    minor code refactor

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,10 +3,13 @@
 def square_matrix_simple(matrix=[]):
     idx_i = 0
     idx_j = 0
-    new_matrix = [[], [], []]
+    len_row = len(matrix)
+    len_col = len(matrix[0])
+    new_matrix = []
     #  ####
-    while (idx_i < 3):
-        while (idx_j < 3):
+    while (idx_i < len_row):
+        new_matrix.append([])
+        while (idx_j < len_col):
             new_matrix[idx_i].append((matrix[idx_i][idx_j])**2)
             idx_j = idx_j + 1
         #  endwhile
