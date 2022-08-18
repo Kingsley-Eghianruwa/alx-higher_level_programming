61ff252f8258d7042989b464d7bc56a2e276d2ff
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-18 14:42:38 +0000

    added 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    check = key in a_dictionary
+    if (check == True):
+        del a_dictionary[key]
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    #  endif
+    return a_dictionary
+#  enddef: update_dictionary
