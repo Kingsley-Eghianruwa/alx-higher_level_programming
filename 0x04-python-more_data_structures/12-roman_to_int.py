4bc5b018ca593227a37f4f5cf192a1ad1cb1d3ae
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-19 16:15:22 +0000

    added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_symbols = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    idx = len(roman_string) - 1
+    cmp_x = 0
+    sum_digits = 0
+    #  ###
+    while (idx >= 0):
+        z = roman_symbols[roman_string[idx]]
+        if (z >= cmp_x):
+            sum_digits = sum_digits + z
+        else:
+            sum_digits = sum_digits - z
+        #  endif
+        cmp_x = z
+        idx = idx - 1
+    #  endwhile
+    return sum_digits
+#  enddef: roman_to_int
