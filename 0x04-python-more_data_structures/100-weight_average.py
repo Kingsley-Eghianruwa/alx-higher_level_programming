65d6c9a9b908772a7b1fc2fb7f73d8469048e1cb
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-20 20:54:14 +0000

    added 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    top = 0
+    bottom = 0
+    idx = 0
+    result = 0
+    #  ###
+    if  (my_list == []):
+        return 0
+    else:
+        max_idx = len(my_list) - 1
+    #  endif
+    #  ###
+    while (idx <= max_idx):
+        tmp = list(my_list[idx])
+        top = top + (tmp[0] * tmp[1])
+        bottom = bottom + tmp[1]
+        idx = idx + 1
+    #  endwhile
+    result = (top * 1.00) / (bottom * 1.00)
+    return result
+#  enddef: weight_average
