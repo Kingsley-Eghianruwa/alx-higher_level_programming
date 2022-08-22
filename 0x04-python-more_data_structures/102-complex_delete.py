2448f96f2014e568a5da1af5299a8b75303afd10
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-22 23:08:35 +0000

    added 102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,14 @@
+#/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    key_list = list(a_dictionary)
+    idx = 0
+    #  ###
+    while (idx < len(key_list)):
+        if (value == a_dictionary[key_list[idx]]):
+            del a_dictionary[key_list[idx]]
+        #  endif
+        idx = idx + 1
+    #  endwhile
+    return a_dictionary
+#  enddef: complex_delete
