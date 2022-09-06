9aa855c796d2b161d85a99c583bc170e9d1fa139
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-09-06 03:24:32 +0000

    styling checked

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,11 +3,11 @@
 const count = Number(process.argv[2]);
 let idx = 1;
 
-if (isNaN(count) || count === undefined){
-	console.log('Missing number of occurrences');
+if (isNaN(count) || count === undefined) {
+  console.log('Missing number of occurrences');
 } else {
-	while (idx <= count){
-		console.log('C is fun');
-		idx = idx + 1;
-	}
+  while (idx <= count) {
+    console.log('C is fun');
+    idx = idx + 1;
+  }
 }
