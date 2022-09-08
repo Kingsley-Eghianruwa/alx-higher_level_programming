d860e6086b93555631507b55155af8607f712c74
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-09-08 02:05:12 +0000

    added 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+function rfact (idx, result) {
+  if (idx === 1) {
+    return result;
+  }
+  result = result * idx;
+  idx = idx - 1;
+
+  return rfact(idx, result);
+}
+
+function fact (n) {
+  const result = 1;
+  const idx = n;
+  return rfact(idx, result);
+}
+
+const value = Number(process.argv[2]);
+
+if (isNaN(value)) {
+  console.log(1);
+} else {
+  const output = fact(value);
+  console.log(output);
+}
