63989c45243e2a9fca9647752ab9466fb83a7d61
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-09-19 00:19:22 +0000

    added 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  consructor () {}
+}
+
+const rectangleMethods = {
+	print() {
+		let idx = 1;
+		let idy = 1;
+		const xhar = "X"
+		let recWidth = "";
+		while (idx <= this.width) {
+			recWidth = recWidth.concat(xhar);
+			idx = idx + 1;
+		}
+		while (idy <= this.height){
+			console.log(recWidth);
+			idy = idy + 1;
+		}
+	},
+};
+
+module.exports = function rectangle (w, h) {
+  const obj = new Rectangle;
+  if (w > 0 && h > 0) {
+    obj.width = w;
+    obj.height = h;
+  }
+	obj.print = rectangleMethods.print;
+  return obj;
+}
