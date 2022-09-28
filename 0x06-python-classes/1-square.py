a24666a7af9de144f01022f2cc9c75abad9d1d32
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-09-28 10:15:37 +0000

    added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" this is a python class module for Square """
+
+
+class Square:
+    """ this is class creates a square object with size """
+
+    def __init__(self,  size):
+        """class square consructor
+
+        size is a private variable
+        """
+        self.__size = size
+    # enddef: __init__
+# endClass: Square
